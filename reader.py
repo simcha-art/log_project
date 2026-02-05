0fe3eb17d0153fa8a498dcac2b8d1104fa4f4244
Simchacop <simchacop@gmail.com>
2026-02-05 17:04:45 +0200

    repair to a list comprehension

--- reader.py
+++ reader.py
@@ -1,13 +1,9 @@
 def read_log_file(file_name) ->list[list]:
     with open(file_name, "r") as f:
         r = f.readlines()
-    matrix = []
-    for row in r:
-        new_row = row.split(",")
-        new_row[-1] = new_row[-1].strip()
-        matrix.append(new_row)
+    matrix = [row.split(",") for row in r]
     return matrix
 
-matrix = read_log_file("network_traffic.log")
-for row in matrix:
+mat = read_log_file("network_traffic.log")
+for row in mat:
     print(row)
