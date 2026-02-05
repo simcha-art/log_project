7be6f54db67db96d54caa6cbc62f214f175ebb2b
Simchacop <simchacop@gmail.com>
2026-02-05 17:11:32 +0200

    func that finds all the outer ips

--- reader.py
+++ reader.py
@@ -5,5 +5,12 @@ def read_log_file(file_name) ->list[list]:
     return matrix
 
 mat = read_log_file("network_traffic.log")
-for row in mat:
-    print(row)
+print(mat[0])
+
+def outer_ips(data:list[list]):
+    arr_ip = [row[1] for row in data if row[1][:2] != "10" and row[1][:7] != "192.168" ]
+    return arr_ip
+
+row = outer_ips(mat)
+for ip in row:
+    print(ip)
