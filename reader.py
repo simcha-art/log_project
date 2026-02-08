76c8b12c448e37ae464bfc74f08fa43bc9d89bbf
Simchacop <simchacop@gmail.com>
2026-02-08 09:14:14 +0200

    add func that finds suspected ports

--- reader.py
+++ reader.py
@@ -11,6 +11,10 @@ def outer_ips(data:list[list]):
     arr_ip = [row[1] for row in data if row[1][:2] != "10" and row[1][:7] != "192.168" ]
     return arr_ip
 
-row = outer_ips(mat)
-for ip in row:
-    print(ip)
+def suspected_ports(data:list[list]):
+    suspected = [row for row in mat if row[3] in ("22", "23", "3389")]
+    return suspected
+
+ports = suspected_ports(mat)
+for row in ports:
+    print(row)
