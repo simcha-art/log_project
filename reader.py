02780e23a2b5b5c870e4986ebd1a7d61021d8e13
Simchacop <simchacop@gmail.com>
2026-02-11 10:34:50 +0200

    update loading the log file to yield

--- reader.py
+++ reader.py
@@ -1,9 +1,7 @@
-import array
 
 
-def read_log_file(file_name) ->list[list]:
+def read_log_file(file_name):
     with open(file_name, "r") as f:
-        r = f.readlines()
-    matrix = [row.split(",") for row in r]
-    return matrix
+        for line in f:
+            yield line.split(",")
 
