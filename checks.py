a94d01adeb8951c7d4db6dd17e67e91803073ebe
Simchacop <simchacop@gmail.com>
2026-02-11 10:51:51 +0200

    add example of using the funcs of read, find_suspicous_lines, write their details in a tuple and count them

--- checks.py
+++ checks.py
@@ -131,5 +131,9 @@ def tuple_line_suspicions(log):
 
 def count_susp_lines(log):
     return sum(1 for line in check_suspicions_log(log))
-
-print(count_susp_lines(mat))
+##5
+lines = read_log_file("network_traffic.log")
+suspicious = check_suspicions_log(lines)
+detailed = tuple_line_suspicions(suspicious)
+count = count_susp_lines(suspicious)
+print(f"Total suspicous: {count}")
